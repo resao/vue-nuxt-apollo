fa00daf39d52703a63f7eb3ff65d835c85f14389
Christopher Miles <mail@christophermiles.co.uk>
2018-06-16 23:30:30 +0100

    Add fruitList component that renders allFruitQuery

---
+++ src/components/FruitList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="all-fruits">
+    <h2>All Fruits</h2>
+    <ul v-if="allFruits">
+      <li v-for="fruit in allFruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
+    </ul>
+  </div>
+</template>
+<script>
+
+import { allFruitQuery } from '@/mixins/allFruitQuery'
+
+export default {
+  mixins: [allFruitQuery]
+}
+</script>
