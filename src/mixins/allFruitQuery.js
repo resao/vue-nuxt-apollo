92b0a00c460128d252547d7496dcf6c5d7bf9b3c
Christopher Miles <mail@christophermiles.co.uk>
2018-06-17 01:58:34 +0100

    Change nasty promise to async / await

--- src/mixins/allFruitQuery.js
+++ src/mixins/allFruitQuery.js
@@ -1,12 +1,13 @@
 import allFruit from '@/graphql/queries/allFruit.gql'
 
 export default {
-  asyncData (context) {
+  async asyncData (context) {
     const client = context.app.apolloProvider.defaultClient
-    return client.query({query: allFruit}).then(({data}) => {
-      return data
-    }).catch(e => {
+    try {
+      const response = await client.query({query: allFruit})
+      return response.data
+    } catch (e) {
       context.error(new Error())
-    })
+    }
   }
 }
