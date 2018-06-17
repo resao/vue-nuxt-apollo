88ad4457187bedb2a46f42de6f72cdf0c48d14c1
Christopher Miles <mail@christophermiles.co.uk>
2018-06-17 01:29:44 +0100

    nuxt asyncData needs to load in smart page component and pass down to dumb component

--- src/components/FruitList.vue
+++ src/components/FruitList.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="all-fruits">
     <h2>All Fruits</h2>
-    <ul v-if="allFruits">
-      <li v-for="fruit in allFruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
+    <ul v-if="fruits">
+      <li v-for="fruit in fruits" :key="fruit.id">{{fruit.name}} <em v-if="fruit.color">{{fruit.color.name}}</em></li>
     </ul>
   </div>
 </template>
+
 <script>
-
-import { allFruitQuery } from '@/mixins/allFruitQuery'
-
 export default {
-  mixins: [allFruitQuery]
+  props: {
+    fruits: {
+      required: true,
+      type: Array
+    }
+  }
 }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    <app-fruit-list></app-fruit-list>
+    <app-fruit-list :fruits="allFruits"/>
   </div>
 </template>
 
 <script>
-import FruitList from '@/components/FruitList.vue'
+import FruitList from '@/components/FruitList'
+import allFruitQuery from '@/mixins/allFruitQuery'
 
 export default {
   name: 'home',
   components: {
     appFruitList: FruitList
-  }
+  },
+  mixins: [allFruitQuery]
 }
 </script>
