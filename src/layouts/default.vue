6f1b3564764da1c81bdd8544981813e46f1d3cae
Christopher Miles <mail@christophermiles.co.uk>
2018-06-17 17:01:31 +0100

    Adding page navigation

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <ul>
+      <li><a href="/">Home (server)</a></li>
+      <li><a href="/fruit">Fruit (server)</a></li>
+      <li><a href="/colours">Colours (server)</a></li>
+    </ul>
+    <ul>
+      <li><nuxt-link to="/">Home (router)</nuxt-link></li>
+      <li><nuxt-link to="/fruit">Fruit (router)</nuxt-link></li>
+      <li><nuxt-link to="/colours">Colours (router)</nuxt-link></li>
+    </ul>
     <nuxt/>
   </div>
 </template>
