6a17b1df11d9e1f21e70acfc56468982de6d9474
Christopher Miles <mail@christophermiles.co.uk>
2018-06-17 12:24:28 +0100

    using vue-apollo to make apollo request, rather than nuxt async

--- src/mixins/allFruitQuery.js
+++ src/mixins/allFruitQuery.js
@@ -1,13 +1,24 @@
-import allFruit from '@/graphql/queries/allFruit.gql'
+import allFruits from '@/graphql/queries/allFruits.gql'
 
 export default {
-  async asyncData (context) {
+  /* async asyncData (context) {
     const client = context.app.apolloProvider.defaultClient
     try {
-      const response = await client.query({query: allFruit})
+      const response = await client.query({query: allFruits})
       return response.data
     } catch (e) {
       context.error(new Error())
+    }
+  } */
+  data: function () {
+    return {
+      allFruits: []
+    }
+  },
+  apollo: {
+    allFruits: {
+      query: allFruits,
+      prefetch: true
     }
   }
 }
