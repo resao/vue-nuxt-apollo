155e0fb76e3f29a04803771199d36883b1ec6464
Christopher Miles <mail@christophermiles.co.uk>
2018-06-17 01:28:50 +0100

    Changed mixin to use nuxt asyncData to ensure apollo client renders server-side

--- src/mixins/allFruitQuery.js
+++ src/mixins/allFruitQuery.js
@@ -1,12 +1,12 @@
 import allFruit from '@/graphql/queries/allFruit.gql'
 
-export const allFruitQuery = {
-  data () {
-    return {
-      allFruits: ''
-    }
-  },
-  apollo: {
-    allFruits: allFruit
+export default {
+  asyncData (context, callback) {
+    const client = context.app.apolloProvider.defaultClient
+    return client.query({query: allFruit}).then(({data}) => {
+      callback(null, data)
+    }).catch(e => {
+      context.error(new Error())
+    })
   }
 }
